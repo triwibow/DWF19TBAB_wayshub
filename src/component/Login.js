bf09ea0ef78cf3d090c92093636f1dee07933de5
triwibow <triw3707@gmail.com>
2020-11-24 16:37:07 +0700

    add login

---
+++ src/component/Login.js
@@ -0,0 +1,21 @@
+import '../App.css';
+import title from  '../title.svg'
+
+function Login(){
+    return(
+        <div className="landing-container">
+            <div className="landing-welcome">
+                <img src={title} alt ="title" />
+                <button className="button">Sign In</button>
+            </div>
+            <div className="landing-form">
+                <h1>Sign In</h1>
+                <input type="text" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+                <button className="button">Sign In</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
