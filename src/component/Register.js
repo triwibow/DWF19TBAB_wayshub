02c5ce9037068766ef58c83621c65569eb80923c
triwibow <triw3707@gmail.com>
2020-11-24 19:12:29 +0700

    add register with conditional rendering

--- src/component/Register.js
+++ src/component/Register.js
@@ -1,23 +1,16 @@
 import '../App.css';
-import title from  '../title.svg'
 
 function Register(){
     return(
-        <div className="landing-container">
-            <div className="landing-welcome">
-                <img src={title} alt ="title" />
-                <button className="button">Sign In</button>
-            </div>
-            <div className="landing-form">
-                <h1>Sign Up</h1>
-                <form>
-                    <input type="text" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <input type="text" placeholder="Name Channel"/>
-                    <textarea placeholder="Description Channel"></textarea>
-                    <button className="button">Sign Up</button>
-                </form>
-            </div>
+        <div className="landing-form">
+            <h1>Sign Up</h1>
+            <form>
+                <input type="text" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+                <input type="text" placeholder="Name Channel"/>
+                <textarea placeholder="Description Channel"></textarea>
+                <button className="button">Sign Up</button>
+            </form>
         </div>
     )
 }
