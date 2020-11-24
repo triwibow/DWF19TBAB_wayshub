9b9bb42ebabea8142253df62bd4d74347648a844
triwibow <triw3707@gmail.com>
2020-11-24 19:03:58 +0700

    add login (add conditional rendering)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, { useState } from 'react';
+import Login from './component/Login';
 import Register from './component/Register';
+import title from  './title.svg';
 
 function App() {
+  const [isLoginPage, setLoginPage] = useState(true);
+
+  const handleLoginPage = () => {
+    (isLoginPage === true)? setLoginPage(false):setLoginPage(true);
+  }
+
   return (
     <div className="App">
-      <Register/>
+      <div className="landing-container">
+            <div className="landing-welcome">
+                <img src={title} alt ="title" />
+                <button className="button" onClick={handleLoginPage}>{(isLoginPage === true)? "Sign Up": "Sign In"}</button>
+            </div>
+            {isLoginPage ? <Login />: <Register />}
+        </div>
     </div>
   );
 }
--- src/component/Login.js
+++ src/component/Login.js
@@ -1,19 +1,14 @@
 import '../App.css';
-import title from  '../title.svg'
 
 function Login(){
     return(
-        <div className="landing-container">
-            <div className="landing-welcome">
-                <img src={title} alt ="title" />
-                <button className="button">Sign Up</button>
-            </div>
-            <div className="landing-form">
-                <h1>Sign In</h1>
+        <div className="landing-form">
+            <h1>Sign In</h1>
+            <form>
                 <input type="text" placeholder="Email"/>
                 <input type="password" placeholder="Password"/>
                 <button className="button">Sign In</button>
-            </div>
+            </form>
         </div>
     )
 }
